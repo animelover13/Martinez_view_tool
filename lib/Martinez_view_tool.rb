bfa0e2e461ad6fabc91f9d8e5feec5bb83309b2c
slcwestdevcamp <slcwest@devcamp.com>
2017-07-05 05:47:19 +0000

    Martinez tool intial commit

---
+++ lib/Martinez_view_tool.rb
@@ -0,0 +1,6 @@
+require "Martinez_view_tool/version"
+require "Martinez_view_tool/renderer"
+
+module MartinezViewTool
+  # Your code goes here...
+end
---
+++ lib/Martinez_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MartinezViewTool
+  VERSION = "0.1.0"
+end
